c9606adb5fb946340d9f7eb115dde750f8795abc
Dane Hillard <github@danehillard.com>
2024-12-01 11:41:18 -0500

    Add day one data and solution

---
+++ python/2024-12-01/main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env -S uv run -q
+# /// script
+# requires-python = ">=3.12"
+# ///
+from collections import Counter
+from csv import reader
+from typing import Generator
+
+
+def _int_reader(csv_reader):
+    for row in csv_reader:
+        yield [int(cell) for cell in row]
+
+
+def _get_data() -> Generator[list[int], None, None]:
+    with open("data.csv") as data_file:
+        for row in _int_reader(reader(data_file)):
+            yield row
+
+
+if __name__ == "__main__":
+    data = _get_data()
+    list_one, list_two = zip(*data)
+
+    # Part one
+    comparable_data = zip(sorted(list_one), sorted(list_two))
+    print(sum(abs(row[0] - row[1]) for row in comparable_data))
+
+    # Part two
+    frequencies = Counter(list_two)
+    print(sum(location_id * frequencies[location_id] for location_id in list_one))
